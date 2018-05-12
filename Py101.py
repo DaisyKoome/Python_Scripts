ab53c1087fe90ce76de88c0dab43ebf13a4eb2de
DaisyKoome <dskoome@gmail.com>
2018-05-12 09:26:15 +0300

    This is python code

---
+++ Py101.py
@@ -0,0 +1,6 @@
+name=input('Please enter your name: ')
+age=input('Please enter your age: ')
+print("The name of the person is",name,"and the age of the person is",age)
+input("Press enter to quit")
+
+
