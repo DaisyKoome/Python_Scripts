10e61304104cbf07ad679914f046d85244ff2800
DaisyKoome <dskoome@gmail.com>
2018-05-12 09:30:46 +0300

    This is python code

---
+++ Py103.py
@@ -0,0 +1,6 @@
+#A program to find out whether a number entered is odd or even
+number=int(input("Please enter any number: "))
+if(number%2==0):
+    print("The number is even")
+else:
+    print("The number is odd")
