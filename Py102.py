e3b9ac99e2e34b80f7811a7852bf76f005727b6f
DaisyKoome <dskoome@gmail.com>
2018-05-12 09:27:54 +0300

    This is python code

---
+++ Py102.py
@@ -0,0 +1,47 @@
+a=10
+if (a==10):
+    print(a)
+    print("This is true")
+
+b=20
+if (a==10):
+    print(a)
+    if (b==20):
+        print(b)
+
+b=20
+if (a==10):
+    print(a)
+    if (b==24):
+        print(b)
+
+a=10
+b=20
+c=30
+if (a==10):
+    print(a)
+elif (b==20):
+    print(b)
+else:
+    print(c)
+
+if (a==40):
+    print(a)
+elif (b==20):
+    print(b)
+else:
+    print(c)
+
+if (a==30):
+    print(a)
+elif (b==70):
+    print(b)
+else:
+    print(c)
+
+if (a==10):
+    print(a)
+    if (b==20):
+            print(b)
+            if (c==30):
+                    print(c)
